fb943c0e5a722a90e7738a2b2d5cbd5d98ef0278
Alexander Sosov <nookat.develop@gmail.com>
2022-01-15 20:05:52 +0300

    chore: rename parameters of function

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,9 +1,9 @@
 (function() {
   const replaceLongText = (selector, maxLength) => {
-    document.querySelectorAll(selector).forEach(desc => {
-      if (desc.textContent.length > maxLength) {
-        const newDesc = desc.textContent.slice(0, maxLength - 2)
-        desc.textContent = newDesc + '...'
+    document.querySelectorAll(selector).forEach(el => {
+      if (el.textContent.length > maxLength) {
+        const newDesc = el.textContent.slice(0, maxLength - 2)
+        el.textContent = newDesc + '...'
       }
     })
   }
